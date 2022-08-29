639eaa8ba3fac46cf41e845963876f85bc889c62
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-08-29 20:50:43 +0530

    Create Use_of_cin.get_in _program.cpp
    to use of cin.get in program in order to print the number and string

---
+++ Use_of_cin.get_in _program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    char a[100];
+    cin.get();
+    cin.getline(a,100);
+    cout<<n<<endl;
+    cout<<a<<endl;
+    return 0;
+}
