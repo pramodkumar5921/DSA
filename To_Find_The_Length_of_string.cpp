b75cfe412ecc96bf2c927f389ee44666a92cccf8
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-08-29 17:44:12 +0530

    Create To_Find_The_Length_of_string.cpp
    to find the length of string of given length

---
+++ To_Find_The_Length_of_string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int length(char *a){
+    int i;
+    for( i=0;a[i]!='\0';i++){
+
+    }
+    return i;
+}
+int main(){
+    char ch;
+    char a[100]="Abodh kumar";
+    int ans=length(a);
+    cout<<"Length of string is =>"<<ans;
+    return 0;
+}
