e442c557f92b87268fdbb54f1024df6aebf49bab
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-08-31 19:16:19 +0530

    Create Print_number_using_recursion.cpp
    print the number in decreasing order
    10
    10 9 8 7 6 5 4 3 2 1

---
+++ Print_number_using_recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n){
+   // base case
+ if(n==0){
+    return ;
+ }
+ // recursive case
+   cout<<n<<" ";
+   print(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
