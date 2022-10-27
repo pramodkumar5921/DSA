c0ed553cf8536586a6b4038cf4ef35562d63ce17
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-10-27 23:06:19 +0530

    Create intersection of the array 2 .cpp

---
+++ intersection of the array 2 .cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums1.size();i++){
+            mp[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.size();i++){
+            auto it=mp.find(nums2[i]);
+            if(it!=mp.end() and it->second>0){
+                ans.push_back(nums2[i]);
+                it->second--;
+            }
+        }
+        return ans;
+    }
+};
