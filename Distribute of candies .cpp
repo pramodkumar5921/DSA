c96cbef4a4db3693c4d078cbe08fd98b5631e882
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-10-29 18:10:13 +0530

    Create Distribute of candies .cpp

---
+++ Distribute of candies .cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n=candyType.size();
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(candyType[i]);
+        }
+        if(st.size()<n/2){
+            return st.size();
+        }
+        return n/2;
+    }
+};
