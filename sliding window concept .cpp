e133749e284eb057c1c78ace9d37313a9687c6d4
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-11-09 21:37:35 +0530

    Create sliding window concept .cpp

---
+++ sliding window concept .cpp
@@ -0,0 +1,16 @@
+int maxsum(int arr[],int n,int k){
+int max_sum=INT_MIN;
+for(int i=0;i<n-k+1;i++){
+int current_sum=0;
+for(int j=0;j<k;j++){
+current_sum=current_sum+arr[i+j];
+max_sum=max(current_sum,max_sum);
+}
+return max_sum;
+}
+int main(){
+int arr[]={1,4,2,10,3,1,0,20};
+int k=4;
+int n=sizeof(arr)/sizeofarr[0]);
+cout<<max_sum(arr,n,k);
+return 0;
