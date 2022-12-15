3b9bd48cec9ca3bc33ab8d9c807c5471e9c1f43b
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2022-12-15 22:42:50 +0530

    Create indentification+format(mcm).cpp

---
+++ indentification+format(mcm).cpp
@@ -0,0 +1,9 @@
+int solve(int arr[],int i,int j){
+  if(i>j){
+    for(int k=i;k<j;k++)[
+      temp ans=solve(arr,i,k),solve(arr,i+1,j);
+      temp=ANS;
+      ANS=FUN(temp ans){
+        return ans;
+      }
+      }
