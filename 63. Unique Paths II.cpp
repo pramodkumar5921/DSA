6676c684f94e4df7fb66300a5077724bcd2dc395
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2023-01-06 18:42:23 +0530

    Create 63. Unique Paths II.cpp

---
+++ 63. Unique Paths II.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n));
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1] == 1){
+            return 0;
+        }
+        // int dp = [m][n];
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0]==0){
+                dp[i][0]=1;
+            }
+            else{
+                break;
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(obstacleGrid[0][j]==0){
+                dp[0][j]=1;
+            }
+            else{
+                break;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
