0160136d9e72c538f164ac1b4a4dd9681f124f77
pramodkumar5921 <112011610+pramodkumar5921@users.noreply.github.com>
2023-01-09 19:08:40 +0530

    Create 149. Max Points on a Line.cpp

---
+++ 149. Max Points on a Line.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& p) {
+        int n=p.size();
+        if(n<=2){
+            return n;
+        }
+        int total=2,maxi=2;
+        // int n=p.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int total=2;
+                for(int k=0;k<n;k++){
+                    if(k!=i and k!=j){
+                        if(((p[j][1]-p[i][1])*(p[k][0]-p[j][0]))==((p[k][1]-p[j][1])*(p[j][0]-p[i][0]))){
+                            total++;
+                        }
+                    }
+                }
+                maxi=max(maxi,total);
+            }
+        }
+        return maxi;
+    }
+};
