5e111140e5d601183d38ce863f8d033fd6c1d23f
pramod kumar <112011610+pramodkumar5921@users.noreply.github.com>
2023-04-25 11:29:05 +0530

    Create C. Bun Lover.cpp

---
+++ C. Bun Lover.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    cout<< 1ll*(n+1)*(n+1)+1<<'\n';
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
