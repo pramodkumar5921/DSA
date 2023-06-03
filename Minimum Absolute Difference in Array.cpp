df97e2935647f896ceb4a3409bb2a944e41357ef
pramod kumar <112011610+pramodkumar5921@users.noreply.github.com>
2023-06-03 09:15:29 +0530

    Create Minimum Absolute Difference in Array.cpp

---
+++ Minimum Absolute Difference in Array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    int ans=INT_MAX;
+    for(int i=1;i<n;i++){
+        ans=min(ans,abs(arr[i]-arr[i-1]));
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
