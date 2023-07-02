420d87efe5b78d9c22d5b21092540715cc8a3c95
pramod kumar <112011610+pramodkumar5921@users.noreply.github.com>
2023-07-02 17:20:39 +0530

    Create convert_to_binary_tree_doubly_link_list.cpp

---
+++ convert_to_binary_tree_doubly_link_list.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node*left;
+    Node*right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+void flatten(Node*root){
+    if(root==NULL || (root->right==NULL and root->left==NULL)){
+        return;
+    }
+    if(root->left!=NULL){
+        flatten(root->left);
+        Node*temp=root->right;
+        root->right=root->left;
+        root->left=NULL;
+
+        Node*t=root->right;
+        while(t->right!=NULL){
+            t=t->right;
+        }
+        t->right=temp;
+    }
+    flatten(root->right);
+    return;
+}
+void inorder(Node*root){
+    if(root==NULL){
+        return ;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+int main(){
+    Node* root=new Node(4);
+    root->left=new Node(9);
+    root->right=new Node(5);
+    root->left->left=new Node(1);
+    root->left->right=new Node(3);
+    root->right->right=new Node(6);
+    flatten(root);
+    inorder(root);
+    return 0;
+}
