9b341c517405e36674ffcd706a706d2f755a18bd
pramod kumar <112011610+pramodkumar5921@users.noreply.github.com>
2024-03-15 22:15:25 +0530

    Create Create 238. Product of Array Except Self_optimize.cpp

---
+++ Create 238. Product of Array Except Self_optimize.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>result(n,0);
+        result[0]=1;
+        for(int i=1;i<n;i++){
+            result[i]=result[i-1]*nums[i-1];
+        }
+        int rightProduct=1;
+        for(int i=n-1;i>=0;i--){
+           result[i]=result[i]*rightProduct;
+           rightProduct*=nums[i];
+        }
+        return result;
+    }
+};
