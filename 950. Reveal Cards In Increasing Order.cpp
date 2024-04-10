5bf64fdb6bb82ba622007638c6ac46d5445713ed
pramod kumar <112011610+pramodkumar5921@users.noreply.github.com>
2024-04-10 18:07:39 +0530

    Create 950. Reveal Cards In Increasing Order.cpp

---
+++ 950. Reveal Cards In Increasing Order.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n=deck.size();
+        vector<int>result(n,0);
+        sort(deck.begin(),deck.end());
+        int j=0;
+        int i=0;
+        bool skip=false;
+        while(i<n){
+            if(result[j]==0){
+                if(skip==false){
+                    result[j]=deck[i];
+                    i++;
+                }
+                 skip = !skip;
+            }
+            j=(j+1)%n;
+        }
+        return result;
+    }
+};
